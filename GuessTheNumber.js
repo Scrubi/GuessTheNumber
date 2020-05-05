e033091d05d167672798fbe9a51e0533b779e2e4
Scrubi <51309968+Scrubi@users.noreply.github.com>
2020-05-05 10:27:43 +0300

    Made few changes to backend

--- GuessTheNumber.js
+++ GuessTheNumber.js
@@ -1,14 +1,14 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+var randomNumber = Math.floor(Math.random() * 100) + 1;
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
-let guessCount = 1;
-let resetButton;
+var guessCount = 1;
+var resetButton;
 
 function checkGuess() {
-  let userGuess = Number(guessField.value);
+  var userGuess = Number(guessField.value);
   if (guessCount === 1) {
     guesses.textContent = "Previous guesses: ";
   }
@@ -57,7 +57,7 @@ function setGameOver() {
 function resetGame() {
   guessCount = 1;
   const resetParas = document.querySelectorAll(".resultParas p");
-  for (let i = 0; i < resetParas.length; i++) {
+  for (var i = 0; i < resetParas.length; i++) {
     resetParas[i].textContent = "";
   }
 
